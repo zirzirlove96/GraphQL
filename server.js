20356700d91e0a7cd4d31ee300a6becb5817387e
김지영 <zirzirlove96@github.com>
2024-05-21 22:07:50 +0900

    graphql2

--- server.js
+++ server.js
@@ -3,8 +3,22 @@ import { ApolloServer, gql } from "apollo-server";
 //data의 형태
 //Rest Api에서는 GET메서드에 /text하는 것과 같다
 const typeDefs = gql`
+  type User {
+    id: ID!
+    username: String!
+  }
+  type Tweet {
+    id: ID!
+    text: String!
+    creator: User!
+  }
   type Query {
-    text: String
+    allTweets: [Tweet!]!
+    tweet(id: ID): Tweet!
+  }
+  type Mutation {
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean!
   }
 `;
 const server = new ApolloServer({ typeDefs });
